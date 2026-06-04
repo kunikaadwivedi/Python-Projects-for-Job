063c042dfdc82ac65f7eb10cd4465187c0c3dc43
Kunikaa Dwivedi <157058146+kunikaadwivedi@users.noreply.github.com>
2026-06-04 13:30:49 +0200

    Create welcome_to_the_jungle.py

---
+++ welcome_to_the_jungle.py
@@ -0,0 +1,42 @@
+name = input("Welcome to the Jungle!! What is your name?").lower()
+print("Welcome to the Jungle", name +"!!")
+print("Here, you will be asked a series of questions about the jungle!!")
+print("Let's get started!!")
+
+answer = input("You are walking through the jungle, it has come to an end and you can go left or right. Which way would you like to go? ").lower()
+
+if answer == "left":
+    answer = input(
+        "You come to a river, you can walk around it or swim accross? Type walk to walk around and swim to swim accross: ")
+
+    if answer == "swim":
+        print("You swam acrross and were eaten by an alligator.")
+    elif answer == "walk":
+        print("You walked for many miles, ran out of water and you lost the game.")
+    else:
+        print('Not a valid option. You lose.')
+
+elif answer == "right":
+    answer = input(
+        "You come to a bridge, it looks wobbly, do you want to cross it or head back (cross/back)? ")
+
+    if answer == "back":
+        print("You go back and lose.")
+    elif answer == "cross":
+        answer = input(
+            "You cross the bridge and meet a stranger. Do you talk to them (yes/no)? ")
+
+        if answer == "yes":
+            print("You talk to the stanger and they give you gold. You WIN!")
+        elif answer == "no":
+            print("You ignore the stranger and they are offended and you lose.")
+        else:
+            print('Not a valid option. You lose.')
+    else:
+        print('Not a valid option. You lose.')
+
+else:
+    print('Not a valid option. You lose.')
+
+print("Thank you for trying", name)
+print("BYE!! BIRDIE!!")
